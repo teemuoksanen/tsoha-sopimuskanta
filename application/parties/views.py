c79f862f60bac8fbb0ae186abf33fb42ca6b078a
Teemu Oksanen <github@teemuoksanen.fi>
2020-05-30 19:29:05 +0300

    Osapuoli-osion parantaminen

--- application/parties/views.py
+++ application/parties/views.py
@@ -12,9 +12,9 @@ def parties_index():
 @app.route("/parties/new/")
 @login_required
 def parties_form():
-    return render_template("parties/new.html", form = PartyForm())
+    return render_template("parties/form.html", form = PartyForm())
 
-@app.route("/parties/bankrupt/<party_id>/", methods=["POST"])
+@app.route("/parties/<party_id>/setbankrupt/", methods=["POST"])
 @login_required
 def parties_set_bankrupt(party_id):
 
@@ -22,7 +22,17 @@ def parties_set_bankrupt(party_id):
     party.bankrupt = True
     db.session().commit()
 
-    return redirect(url_for("parties_index"))
+    return redirect(url_for('parties_view', party_id=party.id))
+
+@app.route("/parties/<party_id>/unsetbankrupt/", methods=["POST"])
+@login_required
+def parties_unset_bankrupt(party_id):
+
+    party = Party.query.get(party_id)
+    party.bankrupt = False
+    db.session().commit()
+
+    return redirect(url_for('parties_view', party_id=party.id))
 
 @app.route("/parties/", methods=["POST"])
 @login_required
@@ -30,7 +40,7 @@ def parties_create():
     form = PartyForm(request.form)
 
     if not form.validate():
-        return render_template("parties/new.html", form = form)
+        return render_template("parties/form.html", form = form)
     
     party = Party(form.name.data)
     party.business_id = form.businessid.data
@@ -43,3 +53,9 @@ def parties_create():
     db.session().commit()
   
     return redirect(url_for("parties_index"))
+
+@app.route("/parties/<int:party_id>/", methods=["GET"])
+@login_required
+def parties_view(party_id):
+    party = Party.query.get(party_id)
+    return render_template("parties/view.html", party = party)
