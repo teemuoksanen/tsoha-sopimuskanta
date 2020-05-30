205a2b37da07082b9c61e6f2b71d5bec788bb287
Teemu Oksanen <github@teemuoksanen.fi>
2020-05-31 00:23:30 +0300

    Osapuolten muokkaaminen mahdollistettu

--- application/parties/forms.py
+++ application/parties/forms.py
@@ -3,11 +3,10 @@ from wtforms import StringField, BooleanField, validators
 
 class PartyForm(FlaskForm):
     name = StringField("Nimi", [validators.Length(min=2, max=144, message="Nimen on oltava %(min)d-%(max)d merkkiä pitkä")])
-    businessid = StringField("Y-tunnus", [validators.Length(min=9, max=9, message="Y-tunnuksen on oltava tasan 9 merkkiä pitkä")])
+    business_id = StringField("Y-tunnus", [validators.Length(min=9, max=9, message="Y-tunnuksen on oltava tasan 9 merkkiä pitkä")])
     address_street = StringField("Lähiosoite", [validators.Length(min=2, max=144, message="Lähiosoitteen on oltava %(min)d-%(max)d merkkiä pitkä")])
     address_postalcode = StringField("Postinumero", [validators.Length(min=5, max=5, message="Postinumeron on oltava tasan 5 merkkiä pitkä")])
     address_city = StringField("Kaupunki", [validators.Length(min=2, max=50, message="Kaupungin on oltava %(min)d-%(max)d merkkiä pitkä")])
-    bankrupt = BooleanField("Konkurssissa")
  
     class Meta:
         csrf = False
--- application/parties/views.py
+++ application/parties/views.py
@@ -9,10 +9,75 @@ from application.parties.forms import PartyForm
 def parties_index():
     return render_template("parties/list.html", parties = Party.query.all())
 
+@app.route("/parties/", methods=["POST"])
+@login_required
+def parties_create():
+    form = PartyForm(request.form)
+
+    if not form.validate():
+        return render_template("parties/form.html", form = form)
+    
+    party = Party(form.name.data)
+    party.business_id = form.business_id.data
+    party.address_street = form.address_street.data
+    party.address_postalcode = form.address_postalcode.data
+    party.address_city = form.address_city.data
+    party.bankrupt = False
+
+    db.session().add(party)
+    db.session().commit()
+  
+    return redirect(url_for("parties_index"))
+
 @app.route("/parties/new/")
 @login_required
 def parties_form():
-    return render_template("parties/form.html", form = PartyForm())
+    form = PartyForm()
+    return render_template("parties/form.html", form = form)
+
+@app.route("/parties/<int:party_id>/", methods=["GET"])
+@login_required
+def parties_view(party_id):
+    party = Party.query.get(party_id)
+    return render_template("parties/view.html", party = party)
+
+@app.route("/parties/delete/<party_id>/", methods=["POST"])
+@login_required
+def parties_delete(party_id):
+
+    party = Party.query.get(party_id)
+
+    db.session().delete(party)
+    db.session().commit()
+
+    return redirect(url_for("parties_index"))
+
+@app.route("/parties/edit/<int:party_id>/", methods=["GET"])
+@login_required
+def parties_edit_form(party_id):
+    party = Party.query.get(party_id)
+    form = PartyForm(obj=party)
+    return render_template("parties/form.html", form = form, action = "edit", party_id = party_id)
+
+@app.route("/parties/edit/<int:party_id>/", methods=["POST"])
+@login_required
+def parties_edit(party_id):
+    form = PartyForm(request.form)
+
+    if not form.validate():
+        return render_template("parties/form.html", form = form, action = "edit")
+
+    editedParty = Party.query.get(party_id)
+    editedParty.name = form.name.data
+    editedParty.business_id = form.business_id.data
+    editedParty.address_street = form.address_street.data
+    editedParty.address_postalcode = form.address_postalcode.data
+    editedParty.address_city = form.address_city.data
+
+    db.session().add(editedParty)
+    db.session().commit()
+  
+    return redirect(url_for('parties_view', party_id=party_id))
 
 @app.route("/parties/<party_id>/setbankrupt/", methods=["POST"])
 @login_required
@@ -33,29 +98,3 @@ def parties_unset_bankrupt(party_id):
     db.session().commit()
 
     return redirect(url_for('parties_view', party_id=party.id))
-
-@app.route("/parties/", methods=["POST"])
-@login_required
-def parties_create():
-    form = PartyForm(request.form)
-
-    if not form.validate():
-        return render_template("parties/form.html", form = form)
-    
-    party = Party(form.name.data)
-    party.business_id = form.businessid.data
-    party.address_street = form.address_street.data
-    party.address_postalcode = form.address_postalcode.data
-    party.address_city = form.address_city.data
-    party.bankrupt = form.bankrupt.data
-
-    db.session().add(party)
-    db.session().commit()
-  
-    return redirect(url_for("parties_index"))
-
-@app.route("/parties/<int:party_id>/", methods=["GET"])
-@login_required
-def parties_view(party_id):
-    party = Party.query.get(party_id)
-    return render_template("parties/view.html", party = party)
