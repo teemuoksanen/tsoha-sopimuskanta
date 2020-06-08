1d161bb27cb1e358f97a8da26ca660fd4da8fcca
Teemu Oksanen <github@teemuoksanen.fi>
2020-06-08 15:01:25 +0300

    Toimenpiteitä vaativat muistutukset etusivulle (ei vielä muotoilua)

--- application/reminders/models.py
+++ application/reminders/models.py
@@ -2,6 +2,7 @@ from application import db
 from application.models import Base
 
 from sqlalchemy.sql import text
+from datetime import date
 
 class Reminder(Base):
 
@@ -29,5 +30,19 @@ class Reminder(Base):
         response = []
         for row in res:
             response.append({"id":row[0], "note":row[1], "date_remind":row[2], "done":row[3], "contract_id":row[4], "contract_name":row[5], "account_id":row[6], "account_username":row[7], "account_name":row[8]})
+
+        return response
+
+    @staticmethod
+    def active_reminders_for_user(account_id):
+        stmt = text("SELECT Reminder.id, Reminder.note, Reminder.date_remind, Reminder.done, Contract.id, Contract.name, Account.id FROM Reminder"
+                    " JOIN Account ON Reminder.account_id = Account.id"
+                    " JOIN Contract ON Reminder.contract_id = Contract.id"
+                    " WHERE (Account.id = :account_id AND Reminder.done = FALSE AND Reminder.date_remind <= :date)").params(account_id=account_id, date = date.today())
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "note":row[1], "date_remind":row[2], "done":row[3], "contract_id":row[4], "contract_name":row[5], "account_id":row[6]})
 
         return response
--- application/views.py
+++ application/views.py
@@ -1,6 +1,14 @@
 from flask import render_template
 from application import app
+from flask_login import current_user
+
+from application.reminders.models import Reminder
+from application.auth.models import User
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if current_user.is_authenticated:
+        active_reminders = Reminder.active_reminders_for_user(current_user.id)
+    else:
+        active_reminders = None
+    return render_template("index.html", active_reminders = active_reminders)
